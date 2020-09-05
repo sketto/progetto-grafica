e3c03e437a6de4436fe506c9eeb1872336084acf
sketto <francesco.schettino@unife.student.it>
2020-09-06 01:30:12 +0200

    spostamento luce

--- main.c
+++ main.c
@@ -1,25 +1,17 @@
 /*
-* Draw a cube ... VBO
-*/
-
+ * OpenGL: lighting and shading
+ */
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include <GL/freeglut_ext.h>
-#include <GL/freeglut_std.h>
 #include <GL/glu.h>
-#include <GL/gl.h>
 #include <GL/glut.h>
 #include <stdio.h>
 #include <math.h>
 
-// number of steps for the interpolation
-#define DELTA   (1.0/20.0)
-// define and initialize initial and final
-// points for the position interpolation
 // define and initialize initial and final
 // points for the position interpolation
 GLfloat startEndPoints[2][3] =
-{{0.0, 0.0, 0.0}, {1.0, 1.0, -1.0}};
+{{-3.0, 1.0, 0.0}, {3.0, 1.0, 0.0}};
 
 // center and radius of circular trajectory
 GLfloat centerPoint[3] = {0.0, 0.0, -3.0};
@@ -35,16 +27,18 @@ GLfloat startEndAngles[2][3] =
 // (alpha,beta,gamma) is equivalent to (180+alpha, -beta+180, 180+gamma)
 GLfloat startEndAngles1[2][3] =
 {{180.0, 120.0, 180.0}, {0.0, 0.0, 0.0}};
+// number of steps for the interpolation
+#define DELTA   (1.0/50.0)
 //current step of the interpolation
 static float currAlpha = 0;
 
-int up, down;
-// display function
+// display routine
 void display(void)
 {
-    fprintf(stderr, "display\n");
     int currInd;
-    // current position and angle.
+    // number of slices and stacks of the glut sphere
+    GLfloat nSlices=100.0, nStacks=100.0;
+ // current position and angle.
     GLfloat currPos[3], currAngle[3], currAngle1[3], currPos2D[2];
     // current position and angle.
     GLfloat lightPos[4];
@@ -54,79 +48,98 @@ void display(void)
     // Material properties
     float matAmbAndDif[] = {1.0, 1.0, 0.0, 1.0};
     float matSpec[] = {0.0, 1.0, 0.0, 1.0};
-    float matShine[] = {10.0};
+    float matShine[] = {25.0};
+
+     // update the position --> prende i valori della matrice startEndPoints e li carica nelle prime tre posizioni del vettore lightPos[4]
+    for(currInd = 0; currInd < 3; currInd++) {
+        lightPos[currInd] = (1-currAlpha)*startEndPoints[0][currInd] +
+        currAlpha * startEndPoints[1][currInd];
+        fprintf(stderr, "lightPos[%d]%f\n", currInd, lightPos[currInd]);
+                        // interpolate first angle
+        currAngle[currInd] = (1-currAlpha)*startEndAngles[0][currInd] +
+            currAlpha * startEndAngles[1][currInd];
+
+        fprintf(stderr, "currAngle[%d]%f\n", currInd, currAngle[currInd]);
+                // interpolate second angle
+        currAngle1[currInd] = (1-currAlpha)*startEndAngles1[0][currInd] +
+            currAlpha * startEndAngles1[1][currInd];
+        fprintf(stderr, "currAngle1[%d]%f\n", currInd, currAngle1[currInd]);
+    }
 
     // position light
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
+    // clear color and depth buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPushMatrix(); //initial push
-//    glLoadIdentity();
-//    gluLookAt(0.0, 0.0, -0.1, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0);
 
-    // update the position
-    for(currInd = 0; currInd < 3; currInd++)
-    {
-        // interpolate first angle
-        currAngle[currInd] = (1-currAlpha)*startEndAngles[0][currInd] +
-                             currAlpha * startEndAngles[1][currInd];
-        // interpolate second angle
-        currAngle1[currInd] = (1-currAlpha)*startEndAngles1[0][currInd] +
-                              currAlpha * startEndAngles1[1][currInd];
-    }
-
+    // Push initial state on the stack
     glPushMatrix();
-    // place object into the scene
-    glTranslatef(0.0, 0.0, 0.5);
-    glRotatef(60.0, 1.0, 0.0, 0.0);
-    // ... and rotate it with the Euler angles
-    // the order is X, then Y, then Z
 
-    glRotatef(currAngle[0], 1.0, 0.0, 0.0);
-    glRotatef(currAngle[1], 0.0, 1.0, 0.0);
-    glRotatef(currAngle[2], 0.0, 0.0, 1.0);
+    // place objects in the scene
+    glTranslatef(0.0, 0.0, -5.0);
+    //glRotatef(90, 1.0, 0.0, 0.0);
 
-    glColor3f(1.0, 0.0, 0.0);
-    glutWireCube(0.5);
-    glPopMatrix();
     // Material properties of the box.
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, matAmbAndDif);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, matSpec);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, matShine);
 
-
     // let there be light
     glEnable(GL_LIGHT0);
 
-    up = 0;
+    // draw sphere
+    glutSolidSphere(1.0, nSlices, nStacks);
+
+
+    gluLookAt(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
+    // flush graphics objects immediately
     glFlush();
 
+    // pop initial state from the stack
+    glPopMatrix();
 }
 
 void init (void)
 {
+    GLenum glErr;
+
     // global ambient light
-    float globAmb[] = {0.0, 0.0, 0.0, 1.0};
+    float globAmb[] = {0.0, 0.0, 0.0, 1.0}; // RGB
 
     // Light property vectors.
-    float lightAmb[] = {0.0, 0.0, 0.0, 1.0};
-    float lightDif[] = {0.0, 0.0, 0.0, 1.0};
-    float lightSpec[] = {0.0, 0.0, 0.0, 1.0};
-    //glClearColor(1.0, 0.0, 0.0, 0.0);
-
-    glFrustum(-0.4, 0.4, -0.4, 0.4, 0.1, 4.0);
+    float lightAmb[] = {0.9, 0.0, 0.0, 1.0};
+    float lightDif[] = {0.9, 0.0, 0.0, 1.0};
+    float lightSpec[] = {0.0, 0.9, 0.0, 1.0};
 
     // select clearing color
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(1.0, 1.0, 1.0, 0.0);
 
     // Enable depth test
     glEnable(GL_DEPTH_TEST);
 
+    // initialize viewing values
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // set viewing frustum
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1, 10.0);
+
+    // ... it does not hurt to check that everything went OK
+    if ((glErr=glGetError()) != 0) {
+        printf("Errore = %d \n", glErr);
+        exit(-1);
+    }
+
+    // initialize model view transforms
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
+    // Turn on wireframe mode (only for debugging purposes)
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
     // Turn on OpenGL lighting.
-    //glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHTING);
 
     // Set properties of light number 0
     glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
@@ -147,52 +160,29 @@ void init (void)
     // set shading model
     //glShadeModel(GL_FLAT);
     glShadeModel(GL_SMOOTH);
-
 }
 
-GLfloat z = 0.0;
-GLfloat x2 = 0.0;
-GLfloat y2 = 0.0;
-GLfloat speed = 0.2;
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-    fprintf(stderr, "keyInput\n");
-    switch(key)
-    {
-    case 27:
-        // ESC
-        exit(0);
-        break;
+    switch(key) {
+        case 27:
+            // ESC
+            exit(0);
+            break;
+        case '1':
+            // Key 1
+            // update alpha and display
+            currAlpha += DELTA;
+            if (currAlpha > 1.0) currAlpha = 0.0;
+            glutPostRedisplay();
+            break;
 
-    case '1':
-
-        gluLookAt(0.0, 0.0, -0.1, 0.0, 0.0, -0.8, -3.0, 0.0, 0.0);
-        glutPostRedisplay();
-        break;
-
-    case 's':
-
-        glPopMatrix();
-        glPushMatrix();
-        glRotatef(-2.0, 1.0, 0.0, 0.0);
-        glutPostRedisplay();
-        break;
-
-    case 'w':
-
-        glPopMatrix();
-        glPushMatrix();
-        glRotatef(2.0, 1.0, 0.0, 0.0);
-        glutPostRedisplay();
-        break;
-
-    default:
-        // do nothing
-        break;
+        default:
+            // do nothing
+            break;
     }
 }
-
 GLfloat angle = 0.0;
 GLfloat rotation = 0.9;
 
@@ -255,39 +245,6 @@ void mouseInput(int button,int state,int x,int y) //da modificare
     }
 }
 
-void SpecialKeys(int key, int x, int y)
-{
-    switch (key)
-    {
-    case GLUT_KEY_LEFT:
-        //doSomething();
-        break;
-    case GLUT_KEY_RIGHT:
-        //doSomething();
-        break;
-
-    case GLUT_KEY_UP:
-
-        glPopMatrix();
-        glPushMatrix();
-
-        glTranslatef(0.0, y2 -= speed, 0.0);
-        fprintf(stderr, "y: %f\n", y2);
-        glutPostRedisplay();
-        break;
-
-    case GLUT_KEY_DOWN:
-
-        glPopMatrix();
-        glPushMatrix();
-
-        glTranslatef(0.0, y2 += speed, 0.0);
-        fprintf(stderr, "y: %f\n", y2);
-        glutPostRedisplay();
-        break;
-    }
-}
-
 // Window size and mode
 int main(int argc, char** argv)
 {
@@ -296,34 +253,28 @@ int main(int argc, char** argv)
 
     // set display mode
     // GLUT_SINGLE = single buffer window
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 
-    glutInitWindowSize (1024, 576);
-    glutInitWindowPosition (100, 100);
-    //glutCreateWindow ("Un saluto dal vostro spidey di quartiere!");
-    glutCreateWindow("Progetto grafica computerizzata");
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow ("OpenGL Window");
 
     // keyboard handling function
     glutKeyboardFunc(keyInput);
-    glutSpecialFunc(SpecialKeys);
+    glutMouseFunc(mouseInput);
 
-    //glutMouseFunc(mouse(xpos, ypos));
     // Here we add support for GLEW
     GLenum err = glewInit();
-    if (GLEW_OK != err)
-    {
+    if (GLEW_OK != err) {
         printf("GLEW init failed: %s\n", glewGetErrorString(err));
         exit(1);
-    }
-    else
-    {
+    } else {
         printf("GLEW init success\n");
     };
 
-    // Call initialization routinesx
+    // Call initialization routines
     init();
     glutDisplayFunc(display);
-    glutMouseFunc(mouseInput);
     glutMainLoop();
     return 0;   /* ANSI C requires main to return int. */
 }
